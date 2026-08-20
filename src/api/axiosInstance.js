06e98ac12436a5ef3e58ffd621e06825c3f6b7f3
Ujjawal Kumar <ujjawalkmr9211@gmail.com>
2026-08-20 21:32:48 +0530

    api optimize start
    Signed-off-by: Ujjawal Kumar <ujjawalkmr9211@gmail.com>

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 import axios from "axios";
-const baseUrl = "http://localhost:5000/api";
-const authBaseUrl = "http://localhost:5001/api";
-const roomBaseUrl = "http://localhost:5005/api";
+
+
+
 export const api = axios.create({
   baseURL: baseUrl,
   timeout: 10000,
@@ -11,14 +12,78 @@ export const api = axios.create({
 //   timeout: 10000,
 // });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  return config;
-});
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(
+      new ApiException({
+        message: error.message,
+      }),
+    );
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    // Successful API response
+    return response;
+  },
+
+  (error) => {
+    if (error.code === "ECONNABORTED") {
+      return Promise.reject(
+        new ApiException({
+          response: null,
+          networkError: false,
+          message: "Request timeout",
+          status: 408,
+        }),
+      );
+    }
+    // API returned response, but status is 4xx/5xx
+    if (error.response) {
+      return Promise.reject(
+        new ApiException({
+          response: error.response.data,
+          networkError: false,
+          message: error.response.data?.message || "API request failed",
+          status: error.response.status,
+        }),
+      );
+    }
+
+    // Request was made but server didn't respond
+    if (error.request) {
+      return Promise.reject(
+        new ApiException({
+          response: null,
+          networkError: true,
+          message: "Network error. Server is not reachable.",
+          status: null,
+        }),
+      );
+    }
+
+    // Something else happened
+    return Promise.reject(
+      new ApiException({
+        response: null,
+        networkError: false,
+        message: error.message || "Request failed",
+        status: null,
+      }),
+    );
+  },
+);
+
 
 //export default api;

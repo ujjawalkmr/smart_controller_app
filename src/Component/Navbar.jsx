96d951268ae1ba45131f0255af70b2558f833e8c
Ujjawal Kumar <ujjawalkmr9211@gmail.com>
2026-08-26 00:29:38 +0530

    logo added
    Signed-off-by: Ujjawal Kumar <ujjawalkmr9211@gmail.com>

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -12,7 +12,8 @@ const Navbar = () => {
       {/* Left: Brand Icon & Logo */}
       <div className="nav-brand" onClick={() => handleClick("Brand / Logo")}>
         <div className="brand-icon">
-          <svg
+          <img src="/assets/images/logo.jpeg" alt="Brand Logo"  />
+          {/* <svg
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
@@ -23,9 +24,9 @@ const Navbar = () => {
             <polygon points="12 2 2 7 12 12 22 7 12 2" />
             <polyline points="2 17 12 22 22 17" />
             <polyline points="2 12 12 17 22 12" />
-          </svg>
+          </svg> */}
         </div>
-        <span className="brand-name">ApexDash</span>
+        <span className="brand-name">WeELEKTRONIC</span>
       </div>
 
       {/* Right: Navigation Links & Login */}
